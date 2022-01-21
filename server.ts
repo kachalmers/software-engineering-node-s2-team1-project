ab5aeb650ce7416910bd7ccc7d4ffab063de8c7f
JoseAnnunziato <jose.annunziato@sonos.com>
2022-01-21 17:02:15 -0500

    add a b

--- server.ts
+++ server.ts
@@ -4,5 +4,9 @@ const app = express();
 app.get('/hello', (req, res) =>
     res.send('Hello World!'));
 
+app.get('/add/:a/:b', (req, res) => {
+    res.send(req.params.a + req.params.b);
+})
+
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
