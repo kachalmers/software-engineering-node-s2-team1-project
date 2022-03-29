01bd8e92b484ec9a5b5d0f137d38316c95275030
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-03-29 13:44:26 -0400

    trying to get working on chrome. Added sameSite to sess

--- server.ts
+++ server.ts
@@ -50,6 +50,7 @@ let sess = {
     resave: true,
     cookie: {
         secure: false,
+        sameSite: "strict"
     }
 }
 /*
@@ -65,6 +66,7 @@ let sess = {
 if (process.env.ENVIRONMENT === 'PRODUCTION') {
     app.set('trust proxy', 1) // trust first proxy
     sess.cookie.secure = true // serve secure cookies
+    sess.cookie.sameSite = "none";
 }
 
 app.use(session(sess));
