3683384b02ee695774d130689f38268b60c6ffc0
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-04-02 17:11:20 -0400

    Added comments

---
+++ models/tuits/Tag.ts
@@ -0,0 +1,11 @@
+/**
+ * @file Declares Tag data type representing the tuit's tag.
+ */
+
+/**
+ * @typedef Tag Represents the tuit's tag.
+ * @property {string} tag Name of the tag.
+ */
+export default class Tag {
+    private tag: string = '';
+}
--- models/tuits/Tuit.ts
+++ models/tuits/Tuit.ts
@@ -1,6 +1,15 @@
-import User from "../users/User";
+/**
+ * @file Declares Tuit data type representing tuit to be posted by a User.
+ */
+import User from '../users/User'
 import Stats from "./Stats";
 
+/**
+ * @typedef Tuit Represents tuit
+ * @property {string} tuit Content of the tuit
+ * @property {User} postedBy The user who posted the tuit
+ * @property {Date} postedOn The date of this tuit posted on
+ */
 export default interface Tuit {
     tuit: string,
     postedBy: User,
