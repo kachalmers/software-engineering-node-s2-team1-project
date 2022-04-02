c9cf51e96df73a6a1444892a708e3a43ae5f7a7a
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-04-02 17:14:08 -0400

    Added comments

--- models/users/Location.ts
+++ models/users/Location.ts
@@ -1,4 +1,13 @@
-export default interface Location {
-    latitude: number,
-    longitude: number
-};
+/**
+ * @file Declares Location data type to represent user's location.
+ */
+
+/**
+ * @typedef Location Represents user's location
+ * @property {number} latitude latitude
+ * @property {number} longitude longitude
+ */
+export default class Location {
+    public latitude: number = 0.0;
+    public longitude: number = 0.0;
+}
--- models/users/MaritalStatus.ts
+++ models/users/MaritalStatus.ts
@@ -1,7 +1,18 @@
+/**
+ * @file Declares MaritalStatus data type which is
+ * an enumeration to represents user's martial status.
+ */
+
+/**
+ * @typedef MaritalStatus represents user's martial status
+ * @property {string} Married "MARRIED"
+ * @property {string} Single "SINGLE"
+ * @property {string} Widowed "WIDOWED"
+ */
 enum MaritalStatus {
-    Married = "MARRIED",
-    Single = "SINGLE",
-    Widowed = "WIDOWED"
-};
+    Married = 'MARRIED',
+    Single = 'SINGLE',
+    Widowed = 'WIDOWED'
+}
 
 export default MaritalStatus;
