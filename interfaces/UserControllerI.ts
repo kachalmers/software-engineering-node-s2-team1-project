be70408692b32f3cd2ae07cbba446176bded76ca
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-04-02 17:09:25 -0400

    Adding functions to user controller interfaces

--- interfaces/UserControllerI.ts
+++ interfaces/UserControllerI.ts
@@ -1,10 +1,15 @@
+/**
+ * @file Declares Controller RESTful Web service API for users resource
+ */
 import {Request, Response} from "express";
 
 export default interface UserControllerI {
-    findAllUsers (req: Request, res: Response): void;
-    findUserById (req: Request, res: Response): void;
-    createUser (req: Request, res: Response): void;
-    updateUser (req: Request, res: Response): void;
-    deleteUser (req: Request, res: Response): void;
-    deleteAllUsers (req: Request, res: Response): void;
-};
+    findAllUsers(req: Request, res: Response): void;
+    findUserById(req: Request, res: Response): void;
+    createUser(req: Request, res: Response): void;
+    deleteUser(req: Request, res: Response): void;
+    updateUser(req: Request, res: Response): void;
+    login(req: Request, res: Response): void;
+    register(req: Request, res: Response): void;
+    deleteUserByUsername(req: Request, res: Response): void;
+}
--- interfaces/UserDaoI.ts
+++ interfaces/UserDaoI.ts
@@ -1,10 +1,15 @@
-import User from "../models/users/User";
+/**
+ * @file Declares API for Users related data access object methods
+ */
+import User from "../models/users/User"
 
 export default interface UserDaoI {
-    findAllUsers (): Promise<User[]>;
-    findUserById (uid: string): Promise<any>;
-    createUser (user: User): Promise<User>;
-    updateUser (uid: string, user: User): Promise<any>;
-    deleteUser (uid: string): Promise<any>;
-    deleteAllUsers (): Promise<any>;
-};
+    findAllUsers(): Promise<User[]>;
+    findUserById(uid: string): Promise<any>;
+    createUser(user: User): Promise<User>;
+    updateUser(uid: string, user: User): Promise<any>;
+    deleteUser(uid: string): Promise<any>;
+    findUserByCredentials(username: string, password: string): Promise<any>;
+    findUserByUsername(username: string): Promise<any>;
+    deleteUserByUsername(username: string): Promise<any>;
+}
