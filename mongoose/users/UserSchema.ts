96c7ca6f66defd2153d2247e30baaa8dbad2de47
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-04-02 17:16:51 -0400

    Added comments and added location schema

--- mongoose/users/UserSchema.ts
+++ mongoose/users/UserSchema.ts
@@ -1,22 +1,42 @@
+/**
+ * @file Implements mongoose schema for users
+ */
 import mongoose from "mongoose";
+import AccountType from "../../models/users/AccountType";
+import MaritalStatus from "../../models/users/MaritalStatus";
+import LocationSchema from "./LocationSchema";
 import User from "../../models/users/User";
+
+/**
+ * @typedef UserSchema Represent users
+ * @property {string} username User's username
+ * @property {string} password User's password
+ * @property {string} firstName User's first name
+ * @property {string} lastName User's last name
+ * @property {string} email User's email
+ * @property {string} profilePhoto User's profile photo
+ * @property {string} headerImage User's headerImage
+ * @property {string} accountType User's account type where AccountType is an enumeration
+ * @property {string} maritalStatus User's marital status where MaritalStatus is an enumeration
+ * @property {string} biography User's biography
+ * @property {Date} dateOfBirth User's date of birth
+ * @property {Date} joined User's joined date
+ * @property {LocationSchema} location User's location
+ */
 const UserSchema = new mongoose.Schema<User>({
-    username: {type: String, required: true, default: `testusername${Date.now()}`},
-    password: {type: String, required: true, default: `testpassword${Date.now()}`},
+    username: {type: String, required: true},
+    password: {type: String, required: true},
     firstName: String,
     lastName: String,
-    email: {type: String, required: true, default: `testemail${Date.now()}`},
+    email: {type: String, required: true},
     profilePhoto: String,
     headerImage: String,
+    accountType: {type: String, default: AccountType.Personal, enum: AccountType},
+    maritalStatus: {type: String, default: MaritalStatus.Single, enum: MaritalStatus},
     biography: String,
     dateOfBirth: Date,
-    accountType: {type: String, enum: ["PERSONAL", "ACADEMIC", "PROFESSIONAL"]},
-    maritalStatus: {type: String, enum: ["MARRIED", "SINGLE", "WIDOWED"]},
-    location: {
-        latitude: Number,
-        longitude: Number
-    },
-    salary: {type: Number, default: 50000}
-}, {collection: "users"});
+    joined: {type: Date, default: Date.now},
+    location: LocationSchema
+}, {collection: 'users'});
 
 export default UserSchema;
