4f52dbeabb4d0f8b39a54995ff6bfd8fde05ffa7
Katie Chalmers <katiechalmers@Katies-MacBook-Pro-2.local>
2022-04-02 17:10:34 -0400

    Updated comments

--- models/bookmarks/Bookmark.ts
+++ models/bookmarks/Bookmark.ts
@@ -1,17 +1,17 @@
 /**
- * @file declares Bookmark data type representing relationship between
- * users and tuits, as in user bookmarked a tuit.
+ * @file Declares Bookmark data type representing relationship between
+ * users and tuits, as in a user bookmarks a tuit.
  */
 import Tuit from "../tuits/Tuit";
 import User from "../users/User";
 
 /**
- * @typedef Bookmark Represents bookmarked relationship between a user and a
- * tuit, as in a user bookmarked a tuit.
+ * @typedef Bookmark Represents bookmarks relationship between a user and a tuit,
+ * as in a user bookmarks a tuit
  * @property {Tuit} tuit Tuit being bookmarked
- * @property {User} likedBy User bookmarking the tuit
+ * @property {User} bookmarkedBy User bookmarking the tuit
  */
 export default interface Bookmark {
     tuit: Tuit,
     bookmarkedBy: User
-};
+}
